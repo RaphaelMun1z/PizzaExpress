a7c811fea268e22e1a0be826a3e9153e66fc67a6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-18 16:55:56 -0300

    Att

--- src/routes/OrderRoutes.ts
+++ src/routes/OrderRoutes.ts
@@ -4,8 +4,12 @@ const router = express.Router()
 // Controller
 import { CreateOrderController } from "../controllers/order/CreateOrderController"
 import { AddItemController } from "../controllers/order/AddItemController"
-import { RemoveOrderController } from "../controllers/order/RemoveOrderController"
+import { SendOrderController } from "../controllers/order/SendOrderController"
+import { FinishOrderController } from "../controllers/order/FinishOrderController"
+import { ListOrdersController } from "../controllers/order/ListOrdersController"
+import { DetailOrderController } from "../controllers/order/DetailOrderController"
 import { RemoveItemController } from "../controllers/order/RemoveItemController"
+import { RemoveOrderController } from "../controllers/order/RemoveOrderController"
 
 // Middlewares
 import { isAuthenticated } from "../middlewares/isAuthenticated"
@@ -13,7 +17,11 @@ import { isAuthenticated } from "../middlewares/isAuthenticated"
 // Routes
 router.post("/", isAuthenticated, new CreateOrderController().handle)
 router.post("/add", isAuthenticated, new AddItemController().handle)
-router.delete("/", isAuthenticated, new RemoveOrderController().handle)
+router.put("/send", isAuthenticated, new SendOrderController().handle)
+router.put("/finish", isAuthenticated, new FinishOrderController().handle)
+router.get("/", isAuthenticated, new ListOrdersController().handle)
+router.get("/detail", isAuthenticated, new DetailOrderController().handle)
 router.delete("/remove", isAuthenticated, new RemoveItemController().handle)
+router.delete("/", isAuthenticated, new RemoveOrderController().handle)
 
 module.exports = router
