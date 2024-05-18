683cb4bbae3611584787ef8967565f5386bc510c
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-17 22:01:16 -0300

    Att

--- src/routes/OrderRoutes.ts
+++ src/routes/OrderRoutes.ts
@@ -3,13 +3,17 @@ const router = express.Router()
 
 // Controller
 import { CreateOrderController } from "../controllers/order/CreateOrderController"
+import { AddItemController } from "../controllers/order/AddItemController"
 import { RemoveOrderController } from "../controllers/order/RemoveOrderController"
+import { RemoveItemController } from "../controllers/order/RemoveItemController"
 
 // Middlewares
 import { isAuthenticated } from "../middlewares/isAuthenticated"
 
 // Routes
 router.post("/", isAuthenticated, new CreateOrderController().handle)
+router.post("/add", isAuthenticated, new AddItemController().handle)
 router.delete("/", isAuthenticated, new RemoveOrderController().handle)
+router.delete("/remove", isAuthenticated, new RemoveItemController().handle)
 
 module.exports = router
