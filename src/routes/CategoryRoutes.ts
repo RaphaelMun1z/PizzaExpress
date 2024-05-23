0feda26c09d04b88d0c2d483db16b98bebcd20ef
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-22 21:54:15 -0300

    Att

--- src/routes/CategoryRoutes.ts
+++ src/routes/CategoryRoutes.ts
@@ -3,11 +3,13 @@ const router = express.Router()
 
 // Controller
 import { CreateCategoryController } from "../controllers/category/CreateCategoryController"
+import { ListCategoryController } from "../controllers/category/ListCategoryController"
 
 // Middlewares
 import { isAuthenticated } from "../middlewares/isAuthenticated"
 
 // Routes
 router.post("/", isAuthenticated, new CreateCategoryController().handle)
+router.get("/", isAuthenticated, new ListCategoryController().handle)
 
 module.exports = router
